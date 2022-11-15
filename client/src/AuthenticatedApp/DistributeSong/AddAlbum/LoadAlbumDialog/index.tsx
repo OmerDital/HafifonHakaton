7eadb55fe1353e2ea752de64231c41b835e86187
omerdital <omerdital@gmail.com>
2022-11-15 16:24:40 +0200

    basic project

---
+++ client/src/AuthenticatedApp/DistributeSong/AddAlbum/LoadAlbumDialog/index.tsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import {
+  Grid, Paper,
+  Stack
+} from '@mui/material';
+import { Album } from '../../../../types/Album';
+import AlbumCardCell from '../AlbumCardCell';
+import useFetch from '../../../../hooks/use-fetch';
+import SortData from '../../../../components/SortData';
+import SearchByNameFilter from '../../../../components/SearchByNameFilter';
+import DialogContainer from '../../../../components/DialogContainer';
+
+interface LoadAlbumDialogProps {
+  open: boolean,
+  handleClose: () => void,
+}
+
+const sortOptions = [
+  { label: 'שם', value: 'name' },
+  { label: 'תיאור', value: 'description' },
+  { label: 'מדינת הוצאה', value: 'country' },
+  { label: 'סוג אלבום', value: 'type' },
+  { label: 'מאזינים לחישוב', value: 'listeners' },
+  { label: 'מגירה\\הוצאה', value: 'purpose' },
+];
+
+const LoadAlbumDialog = ({ open, handleClose }: LoadAlbumDialogProps) => {
+  const [albumsToDisplay, setAlbumsToDisplay] = useState<Album[]>([]);
+  const [albums] = useFetch<Album[]>('albums', []);
+
+  return (
+    <DialogContainer open={open} handleClose={handleClose} maxWidth='md' title='טעינת אלבום'>
+      <Stack direction='row' spacing={2} sx={{ margin: 2 }}>
+        <SearchByNameFilter data={albums as Album[]} setDataToDisplay={setAlbumsToDisplay} />
+        <SortData
+          dataToDisplay={albumsToDisplay}
+          setDataToDisplay={setAlbumsToDisplay}
+          defaultSortBy='name'
+          sortOptions={sortOptions}
+        />
+      </Stack>
+      <Stack spacing={2} sx={{ height: '300px', overflow: 'auto', px: '10px' }}>
+        {albumsToDisplay.map(album => (
+          <Paper
+            key={album.id}
+            elevation={1}
+            sx={{ height: 80, py: '32px', backgroundColor: 'background.default' }}
+            data-testid='album'
+          >
+            <Grid container alignContent='center' height='100%' spacing={1} margin={0}>
+              <AlbumCardCell text={album.name} label='שם' />
+              <AlbumCardCell text={album.description} label='תיאור' />
+              <AlbumCardCell text={album.country} label='מדינת הוצאה' />
+              <AlbumCardCell text={album.type} label='סוג אלבום' />
+              <AlbumCardCell text={album.listeners} label='מאזינים לחישוב' />
+              <AlbumCardCell text={album.purpose} label='מגירה\הוצאה' />
+            </Grid>
+          </Paper>
+        ))}
+      </Stack>
+    </DialogContainer>
+  );
+};
+
+export default LoadAlbumDialog;
---
+++ client/src/AuthenticatedApp/DistributeSong/AddAlbum/index.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import {
+  Button, IconButton, Stack, Typography
+} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import LayersOutlinedIcon from '@mui/icons-material/LayersOutlined';
+import { SideMenuContainer } from '../SideMenu/SideMenuContainer';
+import LoadAlbumDialog from './LoadAlbumDialog';
+import useOpen from '../../../hooks/use-open';
+import CreateAlbumDialog from '../../ManageEntities/Albums/CreateAlbumDialog';
+
+interface AddAlbumProps {
+  handleClose: () => void;
+}
+
+const AddAlbum = ({ handleClose }: AddAlbumProps) => {
+  const [album] = useState();
+  const [isLoadAlbumOpen, handleLoadAlbumClose, handleLoadAlbumOpen] = useOpen();
+  const [isCreateAlbumOpen, handleCreateAlbumClose, handleCreateAlbumOpen] = useOpen();
+
+  return (
+    <SideMenuContainer
+      container
+      direction='column'
+      alignItems='center'
+      width='30%'
+      flexWrap='nowrap'
+    >
+      <IconButton onClick={handleClose} sx={{ alignSelf: 'flex-end' }}>
+        <CloseIcon />
+      </IconButton>
+      <LayersOutlinedIcon sx={{ width: '240px', height: '220px' }} />
+      {!album && <Typography variant='h6'>אין אלבום טעון כעת</Typography>}
+      <Stack direction='row' spacing={2} sx={{ margin: '24px' }}>
+        <Button variant='contained' color='secondary' onClick={handleLoadAlbumOpen}>טען אלבום</Button>
+        <Button variant='contained' onClick={handleCreateAlbumOpen}>צור חדש</Button>
+      </Stack>
+      <LoadAlbumDialog open={isLoadAlbumOpen} handleClose={handleLoadAlbumClose} />
+      <CreateAlbumDialog open={isCreateAlbumOpen} handleClose={handleCreateAlbumClose} />
+    </SideMenuContainer>
+  );
+};
+
+export default AddAlbum;
